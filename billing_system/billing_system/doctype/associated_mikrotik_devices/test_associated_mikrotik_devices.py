c6cb2f8a897b5a4e27f613c2775aeaad6d1a65b8
nelsonmpanju <nelsonnorbert87@gmail.com>
2024-10-05 17:34:24 +0300

    feat: Adding intial Doctypes

---
+++ billing_system/billing_system/doctype/associated_mikrotik_devices/test_associated_mikrotik_devices.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, iota technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAssociatedMikrotikDevices(FrappeTestCase):
+	pass
---
+++ billing_system/billing_system/doctype/mikrotik_api_log/mikrotik_api_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, iota technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mikrotik API Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
